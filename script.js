58b939612ca980a28cef9c687bdf8ecf97a64347
Mahe413 <devagirimahendra@gmail.com>
2024-06-24 18:17:10 +1000

    first upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add any JavaScript functionality here if needed
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document loaded');
+    // Any additional JavaScript code can be added here
+});
