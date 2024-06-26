589a3acafdea48d02e349d41dc1a7f09a81d6ef1
Mahe413 <devagirimahendra@gmail.com>
2024-06-26 12:58:52 +1000

    added pop up for enquires

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-// Add any JavaScript functionality here if needed
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Document loaded');
-    // Any additional JavaScript code can be added here
+    const modal = document.getElementById("contactModal");
+    const btn = document.getElementById("openModal");
+    const span = document.getElementsByClassName("close-button")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "flex";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
